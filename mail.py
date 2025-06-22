647eba985991aa860ce93cb1d13215202e5561ce
YFHD <ethan941106@gmail.com>
2025-06-22 13:09:58 +0800

    Update

--- mail.py
+++ mail.py
@@ -1,4 +1,5 @@
 from argparse import ArgumentParser
+from datetime import datetime
 import json, logging, sys, re, os.path
 
 from google.oauth2.credentials import Credentials
@@ -7,67 +8,113 @@ from googleapiclient.discovery import build, Resource
 from google_auth_oauthlib.flow import InstalledAppFlow
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = [
-  "https://www.googleapis.com/auth/gmail.readonly",
-  "https://www.googleapis.com/auth/gmail.modify"
-]
 
-def buildService(token: str) -> Resource:
-  """Shows basic usage of the Gmail API. Lists the user's Gmail labels."""
 
-  creds = None
-  # The file token.json stores the user's access and refresh tokens, and is
-  # created automatically when the authorization flow completes for the first
-  # time.
-
-  if not token: 
-    if not os.environ.get("token"):
-      return logging.error("Environment variable: \"token\" not found")
-    
-    logging.info("Environment variable: \"token\" found, using it to login...")
-    data = json.loads(os.environ["token"])
-    creds = Credentials.from_authorized_user_info(data, SCOPES)
+class Services:
+  SCOPES = [
+    "https://www.googleapis.com/auth/gmail.readonly",
+    "https://www.googleapis.com/auth/gmail.modify"
+  ]
   
-  elif os.path.exists(token):
-    logging.info(f"File: \"{token}\" found, using it to login...")
-    creds = Credentials.from_authorized_user_file(token, SCOPES)
+  def __init__(self, authData: dict):
+    creds = Credentials.from_authorized_user_info(authData, Services.SCOPES)
 
-  if creds and creds.expired and creds.refresh_token:
-    creds.refresh(Request())
-  # Call the Gmail API
-  return build("gmail", "v1", credentials=creds)
+    # Refresh credential if needed
+    if creds and creds.expired and creds.refresh_token:
+      creds.refresh(Request())
 
-def fetchCode(service: Resource, timestamp: int) -> int:
-  msgRes: Resource = service.users().messages()
-  allMsg = msgRes.list(userId="me", labelIds=["CATEGORY_UPDATES"], q=f"after:{timestamp}").execute()
+    self.creds = creds
 
-  for msgId in map(lambda x: x["id"], allMsg.get("messages", [])):
-    msg = msgRes.get(userId="me", id=msgId).execute()
-    subject = list(filter(lambda e: e["name"] == "Subject", msg["payload"]["headers"]))
+    self.service = build("gmail", "v1", credentials=self.creds)
+    return
 
-    # Skip mail if subject field not found
-    if not len(subject): continue 
+  def fetchCode(self, after: datetime) -> int:
+    messages_resource: Resource = self.service.users().messages()
+
+    vaild_messages = messages_resource.list(
+      userId="me",
+      labelIds=["CATEGORY_UPDATES"],
+      q=f"after:{int(after.timestamp())}"
+    ).execute()
+
+    logging.info(f"Filtering {vaild_messages.get('resultSizeEstimate', 0)} possible email...")
+
+    for msgId in map(lambda x: x["id"], vaild_messages.get("messages", [])):
+      msg = vaild_messages.get(userId="me", id=msgId).execute()
+      subject = list(filter(lambda e: e["name"] == "Subject", msg["payload"]["headers"]))
+
+      # Skip mail if subject field not found
+      if not len(subject): continue 
+      
+      # Skip mail if it doesn't match No-IP verification mail pattern
+      if not re.match(r"No-IP Verification Code: \d{6}", subject[0]["value"]): continue
+
+      # Move used verification mail to trash 
+      vaild_messages.trash(userId="me", id=msgId).execute()
+      
+      return re.findall(r"\d{6}", subject[0]["value"])[0] # Return matched code
+
+  @staticmethod
+  def authoirzeFlow(credential: dict) -> dict:
+    flow = InstalledAppFlow.from_client_config(credential, Services.SCOPES)
+    creds = flow.run_local_server()
+
+    return creds.to_json()
+
+
+# def buildService(token: str) -> Resource:
+#   """Shows basic usage of the Gmail API. Lists the user's Gmail labels."""
+
+#   creds = None
+#   # The file token.json stores the user's access and refresh tokens, and is
+#   # created automatically when the authorization flow completes for the first
+#   # time.
+
+#   if not token: 
+#     if not os.environ.get("token"):
+#       return logging.error("Environment variable: \"token\" not found")
     
-    # Skip mail if it doesn't match No-IP verification mail pattern
-    if not re.match(r"No-IP Verification Code: \d{6}", subject[0]["value"]): continue
+#     logging.info("Environment variable: \"token\" found, using it to login...")
+#     data = json.loads(os.environ["token"])
+#     creds = Credentials.from_authorized_user_info(data, SCOPES)
+  
+#   elif os.path.exists(token):
+#     logging.info(f"File: \"{token}\" found, using it to login...")
+#     creds = Credentials.from_authorized_user_file(token, SCOPES)
 
-    # Move used verification mail to trash 
-    msgRes.trash(userId="me", id=msgId).execute()
-    
-    return re.findall(r"\d{6}", subject[0]["value"])[0] # Return matched code
+  
+#   # Call the Gmail API
+  
 
 def main():
+  logging.basicConfig(
+    datefmt = '%Y/%m/%d %I:%M:%S',
+    format = '[%(asctime)s] [%(levelname)s] %(message)s',
+    level = logging.INFO
+  )
+
   parser = ArgumentParser()
-  parser.add_argument("-cred", "--credential", help="Gmail API credential json path", default=".\\credentials.json")
+
+  parser.add_argument("-cred", "--credential", help="Gmail API credential json path")
   args = parser.parse_args()
 
-  flow = InstalledAppFlow.from_client_secrets_file(args.credential, SCOPES)
-  creds = flow.run_local_server(port=0)
+  if sys.gettrace() is not None:
+    logging.info("Debug environment detected. Using credentials.json as path")
+    args.credential = "credentials.json"
 
-  logging.info("Login requests detected !")
-  with open("token.json", "w") as token:
-    token.write(creds.to_json())
+  with open(args.credential, "r") as file:
+    cred = json.load(file)
+
+  token = Services.authoirzeFlow(cred)
+
+  with open("token.json", "w") as file:
+    json.dump(token, file)
+
   logging.info("Your token has been written into current folder.")
+
   return 0
 
-if __name__ == "__main__": sys.exit(main())
+  
+
+if __name__ == "__main__": 
+  sys.exit(main())
--- noip-renew.py
+++ noip-renew.py
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from datetime import datetime, timedelta
+import json
 import logging
 import re, os, sys, time
 from argparse import ArgumentParser
@@ -26,14 +28,55 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service as ChromeService
 
-from mail import buildService, fetchCode
+from mail import Services
 
 LOGIN_URL = "https://www.noip.com/login"
 HOST_URL = "https://my.noip.com/dynamic-dns"
 USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:64.0) Gecko/20100101 Firefox/64.0"
 
+class Host:
+  def __init__(self, name: str, expDays: int, confirmButton: 'WebElement'):
+    self.name = name
+    self.expDays = expDays
+    self.confirmButton = confirmButton
+
+  @staticmethod
+  def fromTrWebElement(tr: 'WebElement') -> 'Host':
+
+    def _fetchName(e: 'WebElement') -> str:
+      return e \
+        .find_element(By.XPATH, r".//a[@class='link-info cursor-pointer notranslate']") \
+        .text
+    
+    def _fetchExpDays(e: 'WebElement') -> int:
+      matches = e.find_elements(By.XPATH, ".//a[@class='no-link-style']")
+      if not matches: return 0
+      return int(re.search(r"\d+", matches[0].text).group())
+    
+    def _fetchButton(e: 'WebElement'):
+      button = e.find_elements(By.XPATH, f'.//td[6]/button[1]')
+
+      if button and button[0].text == "Confirm":
+        return button[0]
+        
+      logging.info(f"No host 'confirm' button found")
+      
+      return None
+    
+    return Host(
+      name = _fetchName(tr),
+      expDays = _fetchExpDays(tr),
+      confirmButton= _fetchButton(tr, )
+    )
+  
+  @staticmethod
+  def fromWebElement(tr: 'list[WebElement]') -> list['Host']:
+    return [
+      Host.fromTrWebElement(e) for e in tr
+    ]
+
 class Robot:
-  def __init__(self, username: str, password: str, token: str, headless: bool, verbose: bool):
+  def __init__(self, username: str, password: str, token: str, headless: bool):
     self.token = token
     self.username = username
     self.password = password
@@ -52,69 +95,102 @@ class Robot:
     if headless:
       options.add_argument("--headless")  # If running in a headless environment
     
-    if verbose:
-      options.add_argument("--verbose")
+    # if verbose:
+    #   options.add_argument("--verbose")
 
     if 'https_proxy' in os.environ:
       options.add_argument(f"proxy-server={os.environ['https_proxy']}")
 
-
     self.browser = webdriver.Chrome(options=options, service=ChromeService(log_output="chromedriver.log"))
     self.browser.set_page_load_timeout(90) # Extended timeout for Raspberry Pi.
-    # self.browser.delete_all_cookies()
+    self.browser.delete_all_cookies()
 
+  def checkLogin(self):
+    try:
+      WebDriverWait(self.browser, 30).until(EC.presence_of_element_located((By.ID, "main-menu-toggle")))
+    except:
+      return False
+
+    return True
+  
+  def fetchCode(self, maxTries: int):
+    # Past 10 minutes email is acceptable
+    vaild_time = datetime.now() - timedelta(minutes=10)
+
+    service = Services(self.token)
+    
+    if not service:
+      logging.error(f"Gmail API service not being built, script will end here.")
+      return
+    
+    logging.info(f"Gmail API service built, start fetching emails...")
+
+    while (maxTries > 0):
+      logging.info(f"Attempting to get verification code from Gmail API ({maxTries})")
+      code = service.fetchCode(vaild_time)
+
+      if code: return code
+      maxTries -= 1
+
+    logging.warning(f"Failed to get verification code (timeout)")
+    return None
+  
   def login(self):
-    logging.info(f"Opening {LOGIN_URL}...")
+    logging.info(f"Navigating to {LOGIN_URL}")
     self.browser.get(LOGIN_URL)
-
-    logging.info("Logging in...")
+    
+    logging.info("Sending username and password to the entry")
     ele_usr = self.browser.find_element(By.NAME ,"username")
     ele_pwd = self.browser.find_element(By.NAME, "password")
     ele_usr.send_keys(self.username)
     ele_pwd.send_keys(self.password)
 
+    logging.info("Login button clicked")
     self.browser.find_element(By.ID, "clogs-captcha-button").click()
 
-    if "noip.com/2fa/verify" in self.browser.current_url:
-      attempts = 0
+    # if LOGIN_URL in self.browser.current_url:
+    #   logging.debug("Login failed with wrong username or password")
+    #   raise Exception("Login failed with wrong username or password")
+    
+    if self.checkLogin():
+      return
 
-      # Past 10 minutes email is acceptable
-      now = int(time.time() - 600) 
-      service = buildService(self.token)
-      
-      if not service:
-        return logging.error(f"Gmail API service not being built, script will end here.")
-      
-      logging.info(f"Gmail API service built, start fetching emails...")
+    WebDriverWait(self.browser, 20).until(
+      EC.element_to_be_clickable((By.ID, "ManualSubmitMfa")))
+    
+    logging.info("2FA challenge entered, fetching the 2FA code...")
+    code = self.fetchCode(maxTries=30)
 
-      while True:
-        attempts += 1
-        if attempts > 30:
-          logging.warning(f"Failed to get verification code (timeout)")
-          return None
-        
-        logging.info(f"Attempting to get verification code from Gmail API ({attempts})")
-        code = fetchCode(service, now)
+    logging.info(f"Successfully got the verification code !")
+    input2fa = self.browser.find_element(By.ID, "otp-input").find_elements(By.TAG_NAME, "input")
+    
+    for index, element in enumerate(input2fa):
+      element.send_keys(str(code)[index])
 
-        if code: break # Exit check loop if code is vaild
-        time.sleep(5) # Prevent too many requests 
-        
-        # # Click "resend email" button if it's enabled
-        # resendBtn = self.browser.find_element(By.ID, "resend")
-        # if (resendBtn.is_enabled()) : resendBtn.click()
+    logging.info(f"Current login URL = {self.browser.current_url}")
 
-      logging.info(f"Successfully got the verification code !")
-      input2fa = self.browser.find_element(By.ID, "otp-input").find_elements(By.TAG_NAME, "input")
-      
-      for index, element in enumerate(input2fa):
-        element.send_keys(str(code)[index])
+    loginBtn = WebDriverWait(self.browser, 20).until(
+      EC.element_to_be_clickable((By.NAME, "submit")))
 
-      logging.info(f"Current login URL = {self.browser.current_url}")
-
-      loginBtn = WebDriverWait(self.browser, 20).until(
-        EC.element_to_be_clickable((By.NAME, "submit")))
+    self.browser.execute_script("arguments[0].click();", loginBtn)
   
-      self.browser.execute_script("arguments[0].click();", loginBtn)
+    # while True:
+    #   attempts += 1
+    #   if attempts > 30:
+    #     logging.warning(f"Failed to get verification code (timeout)")
+    #     return None
+      
+    #   logging.info(f"Attempting to get verification code from Gmail API ({attempts})")
+    #   code = service.fetchCode(vaild_time)
+
+    #   if code: break # Exit check loop if code is vaild
+    #   time.sleep(5) # Prevent too many requests 
+      
+      # # Click "resend email" button if it's enabled
+      # resendBtn = self.browser.find_element(By.ID, "resend")
+      # if (resendBtn.is_enabled()) : resendBtn.click()
+
+      
 
       # loginBtn.click()
     
@@ -130,20 +206,17 @@ class Robot:
     return True
 
   def updateHosts(self):
+
+    self._navigateAndWaitFor(HOST_URL, (By.XPATH, '//*[@id="host-panel"]/table/tbody/tr'))
+
+    hosts = Host.fromWebElement(
+      self.browser.find_elements(By.XPATH, r'//*[@id="host-panel"]/table/tbody/tr')
+    )
+
     count = 0
-    next_renewal = []
 
-    self.__openHostsPage()
-    hosts = self.fetchHosts()
-    for index, host in enumerate(hosts):
-      hostLink = self.fetchHostLink(host) # This is for if we wanted to modify our Host IP.
-      hostBtn = self.fetchHostButton(host, index+1) # This is the button to confirm our free host
-      expDays = self.fetchHostExpirationDays(host)
-      hostName = hostLink.text
-      next_renewal.append(expDays)
-      logging.info(f"{hostName} expires in {str(expDays)} days")
-
-      if self.updateHost(hostBtn, hostName):
+    for host in hosts:
+      if self.updateHost(host.confirmButton, host.name):
         count += 1
       
     self.browser.save_screenshot("results.png")
@@ -151,15 +224,14 @@ class Robot:
 
     return True
 
-  def __openHostsPage(self):
-    logging.info(f"Opening {HOST_URL}...")
-    self.browser.get(HOST_URL)
+  def _navigateAndWaitFor(self, url: str, locator: tuple[str, str]):
+    self.browser.get(url)
     try: 
-      WebDriverWait(self.browser, 30).until(EC.presence_of_element_located((By.CLASS_NAME,'table-striped-row')))
+      WebDriverWait(self.browser, 30).until(EC.presence_of_element_located(locator))
     except TimeoutException:
-      logging.error(f"Timeout to wait for element \"table-striped-row\", host page may not load properly\nScreenshot: {self.browser.get_screenshot_as_base64()}")
+      logging.error(f"Timeout for waiting element '{locator[1]}', page may not load properly\nScreenshot: {self.browser.get_screenshot_as_base64()}")
       return
-    logging.info("Host page loaded successfully")
+    logging.info(f"Page '{url}' loaded successfully")
 
     # print("I wanna close")
     # time.sleep(1000)
@@ -185,25 +257,10 @@ class Robot:
 
     self.browser.save_screenshot(f"{hostName}_success.png")
 
-  @staticmethod
-  def fetchHostExpirationDays(host: 'WebElement'):
-    matches = host.find_elements(By.XPATH, ".//a[@class='no-link-style']")
-    if not len(matches): return 0
-    return int(re.search(r"\d+", matches[0].text).group())
-
-  @staticmethod
-  def fetchHostLink(host: 'WebElement'):
-    return host.find_element(By.XPATH, r".//a[@class='link-info cursor-pointer notranslate']")
-
-  @staticmethod
-  def fetchHostButton(host: 'WebElement', index: 'int'):
-    button = host.find_elements(By.XPATH, f"""//*[@id="host-panel"]/table/tbody/tr[{index}]/td[6]/button[1]""")
-    if not len(button): return logging.info("Host \"confirm\" button not found")
-
-    if button[0].text != "Confirm": return None
-    return button[0]
-
   def fetchHosts(self):
+    # Navigate to hosts list URL
+    self._navigateAndWaitFor(HOST_URL, (By.CLASS_NAME,'table-striped-row'))
+
     host_tds = self.browser.find_elements(By.XPATH, "//td[@data-title=\"Host\"]")
     if len(host_tds) == 0:
       raise Exception("No hosts or host table rows not found")
@@ -213,6 +270,8 @@ class Robot:
     self.login()
     self.updateHosts()
     self.browser.quit()
+
+
 
 def main():
   parser = ArgumentParser()
@@ -226,26 +285,50 @@ def main():
 
   args = parser.parse_args()
 
-  if not ( (args.username and args.password) or (args.environment_variable) ):
-    if not (args.username or args.password or args.environment_variable):
-      parser.error("Please provide your login information with [\"-u\" and \"-p\"] or [\"-e\"] ")
-
-    if args.password ^ args.username: 
-      parser.error("Please provide both username and password")
-    
-  token = args.token_path if not args.environment_variable else None
-  username = args.username if (args.username) else os.environ.get("username")
-  password = args.password if (args.password) else os.environ.get("password")
-
   logging.basicConfig(
     datefmt = '%Y/%m/%d %I:%M:%S',
     format = '[%(asctime)s] [%(levelname)s] %(message)s',
-    level = logging.DEBUG if args.verbose else logging.INFO
+    level = logging.INFO
   )
 
-  if not (username and password):
-    parser.error("Environment variables for username and password not found")
+  if sys.gettrace() is not None:
+    logging.info("Debug environment detected. Using environment variable to execute the script.")
+    args.verbose = True
+    args.headless = False
+    args.environment_variable = True
+  
+  username: str
+  password: str
+  token: dict
 
-  return Robot(username, password, token, args.headless, args.verbose).renew()
+  def _fetchEnvOrRaise(env: str) -> str:
+    val = os.environ.get(env)
+    if val != None:
+      return val
+    parser.error(f"Environment variable {env} not found, exiting")
+    
+  if args.environment_variable:
+    username = _fetchEnvOrRaise("username")
+    password = _fetchEnvOrRaise("password")
+    token = json.loads( _fetchEnvOrRaise("token") )
 
-if __name__ == "__main__": sys.exit(main())
+  else:
+    if not args.username:
+      parser.error("Please provide username with -u")
+
+    if not args.password:
+      parser.error("Please provide password with -p")
+
+    if not args.token_path:
+      parser.error("Please provide token path with -t")
+    
+    username = args.username
+    password = args.password
+    with open(args.token_path, "r") as f:
+      token = json.load(f)
+
+  robot = Robot(username, password, token, args.headless)
+  return robot.renew()
+
+if __name__ == "__main__": 
+  sys.exit(main())
